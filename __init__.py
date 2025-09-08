744fa8b4de8c17b64891788b66c1ce60fb99414b
axushgp <ayush31377@gmail.com>
2025-09-08 23:22:05 +0530

    init add

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
 from flask import Flask 
 
+from .routes import main
+
+# a func for creating the app, is better than just inititalizing the app! 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
+    app.config.from_pyfile(config_file)
 
+    app.register_blueprint(main)
 
     return app
