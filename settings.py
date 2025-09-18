04cdb2281f7d7b7bd52fd9634e2e656acd230239
axushgp <ayush31377@gmail.com>
2025-09-18 23:54:49 +0530

    updating settings

--- settings.py
+++ settings.py
@@ -1,1 +1,11 @@
 #this will be the config file for main app.. for calling in all the imp configs
+
+import os
+
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+
+
+
+#this takes in the API key from the env variables
+# basically, this setting file will be called as the config file for the main app...
+# & other imp configs can be added here if needed
